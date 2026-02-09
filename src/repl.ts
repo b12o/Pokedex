036f2dcfb06c5f149550787ac498e5371d93caff
b12o <barny@edubio.de>
2026-02-09 23:48:29 +0100

    basic repl implementation

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,5 @@
+import readline from "node:readline";
+
 export function cleanInput(input: string): string[] {
   if (!input.trim().length) return [];
   const trimmed = input.trim();
@@ -5,4 +7,24 @@ export function cleanInput(input: string): string[] {
     .split(" ")
     .filter((item) => item.trim().length !== 0)
     .map((item) => item.toLowerCase());
+}
+
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.on("line", (input: string) => {
+    const cleaned = cleanInput(input);
+    if (!cleaned.length) {
+      rl.prompt();
+      return;
+    }
+    console.log(`Your command was: ${cleaned[0]}`);
+    rl.prompt();
+  });
+
+  rl.prompt();
 }
