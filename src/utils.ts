d368e37b21eeb8f39147e94a4cb73e24c102afd3
b12o <barny@edubio.de>
2026-02-11 23:49:40 +0100

    add sleep util function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function isEmpty(s: string): boolean {
   return s.trim().length === 0;
 }
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
