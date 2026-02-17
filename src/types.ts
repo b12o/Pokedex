ff969139af13b45e523d751049d2f3888b7b4670
b12o <barny@edubio.de>
2026-02-17 19:16:53 +0100

    remove unneeded fields

--- src/types.ts
+++ src/types.ts
@@ -22,29 +22,9 @@ export type LocationArea = {
   id: number;
   name: string;
   game_index: number;
-  encounter_method_rates: {
-    encounter_method: {
-      name: string;
-      url: string;
-    };
-    version_details: {
-      rate: number;
-      version: {
-        name: string;
-        url: string;
-      };
-    }[];
-  }[];
   location: {
     name: string;
     url: string;
-  };
-  names: {
-    name: string;
-    language: {
-      name: string;
-      url: string;
-    }[];
   };
   pokemon_encounters: {
     pokemon: {
@@ -52,7 +32,6 @@ export type LocationArea = {
       url: string;
     };
   }[];
-  test: boolean;
 };
 
 export type ErrorResponse = {
